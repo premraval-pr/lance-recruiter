d456594c062c3f1b608936f41205aef7e14195be
Prem Raval <50078718+premraval1710@users.noreply.github.com>
2021-06-18 23:16:01 -0400

    SignUp Form commit

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,20 +1,36 @@
 import { Container, Form, Row, Col, Button } from "react-bootstrap";
 import styled from "styled-components";
+import GooglePlacesAutoComplete from "react-google-places-autocomplete";
+import { useState } from "react";
 
 const Styles = styled.div``;
 
 const SignUp = () => {
+    const [formData, setFormData] = useState({
+        firstName: "",
+        lastName: "",
+        companyName: "",
+        email: "",
+        industries: "",
+    });
+    const [location, setLocation] = useState(null);
+    const { firstName, lastName, companyName, email, industries } = formData;
+
     return (
         <Container id="signup">
             <Styles>
-                <div className="text-center my-5 py-5">
-                    <p className="h2">SignUp to get Early Access </p>
+                <div className="my-5 py-3">
+                    <p className="h2 text-center">
+                        SignUp to get Early Access{" "}
+                    </p>
                     <Form className="my-5">
                         <Row>
                             <Col>
                                 <Form.Control
                                     className="my-2"
                                     placeholder="Enter First Name"
+                                    name="firstName"
+                                    value={firstName}
                                     required
                                 />
                             </Col>
@@ -22,6 +38,8 @@ const SignUp = () => {
                                 <Form.Control
                                     className="my-2"
                                     placeholder="Enter Last Name"
+                                    name="lastName"
+                                    value={lastName}
                                     required
                                 />
                             </Col>
@@ -32,11 +50,72 @@ const SignUp = () => {
                                     className="my-2"
                                     placeholder="Enter Email"
                                     type="email"
+                                    name="email"
+                                    value={email}
                                     required
                                 />
                             </Col>
                         </Row>
                         <Row>
+                            <Col>
+                                <GooglePlacesAutoComplete
+                                    selectProps={{
+                                        location,
+                                        onChange: setLocation,
+                                    }}
+                                />
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <Row>
+                                    <Col className="text-muted ml-3 my-2">
+                                        Industry (Select all that apply)
+                                    </Col>
+                                </Row>
+                                <Row className="my-2">
+                                    <Col className="ml-2">
+                                        <Form.Check label="Technical" />
+                                    </Col>
+                                    <Col className="ml-2">
+                                        <Form.Check label="Pharmaceutical" />
+                                    </Col>
+                                    <Col className="ml-2">
+                                        <Form.Check label="IT - Helpdesk" />
+                                    </Col>
+                                    <Col className="ml-2">
+                                        <Form.Check label="Retail" />
+                                    </Col>
+                                    <Col className="ml-2">
+                                        <Form.Check label="Finance" />
+                                    </Col>
+                                    <Col className="ml-2">
+                                        <Form.Check label="Hospitality" />
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col className="ml-2">
+                                        <Form.Check label="Sales" />
+                                    </Col>
+                                    <Col className="ml-2">
+                                        <Form.Check label="Marketing" />
+                                    </Col>
+                                    <Col className="ml-2">
+                                        <Form.Check label="Engineering" />
+                                    </Col>
+                                    <Col className="ml-2">
+                                        <Form.Check label="Healthcare" />
+                                    </Col>
+                                    <Col className="ml-2">
+                                        <Form.Check label="Legal" />
+                                    </Col>
+                                    <Col className="ml-2">
+                                        <Form.Check label="Other" />
+                                    </Col>
+                                </Row>
+                            </Col>
+                        </Row>
+                        <Row className="my-2">
                             <Col>
                                 <Button
                                     style={{
